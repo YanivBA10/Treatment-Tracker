f3e12f37c5ed2290307c58c4e9efc9bb2c1ba2b9
YanivBA10 <yanivba10@gmail.com>
2026-09-10 15:25:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='personal-tracker-v4-1-final-20260906';
+const CACHE='personal-tracker-v5-ux1-20260910';
 const ASSETS=['./','./index.html','./config.js','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
