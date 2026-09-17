0f60471a4ab03a3f820990a53ef1f987d762016d
YanivBA10 <yanivba10@gmail.com>
2026-09-17 23:42:18 +0300

    Load task UX polish 5.6.12

--- config.js
+++ config.js
@@ -8,5 +8,6 @@ window.addEventListener('DOMContentLoaded',()=>{
   load('backup.js?v=5.6.2');
   load('ux-5.6.3.js?v=5.6.3');
   load('ui-5.6.4.js?v=5.6.7');
-  load('tracker-ux-5.6.8.js?v=5.6.8');
+  load('tracker-ux-5.6.8.js?v=5.6.10');
+  load('task-ux-5.6.12.js?v=5.6.12');
 });
