2a5c510dc258be4e7a01e8dd3b74b16058398ad2
YanivBA10 <yanivba10@gmail.com>
2026-09-24 20:04:11 +0300

    Load per-view bottom clearance fix

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ document.documentElement.classList.add('pt-ui-booting');
 
 window.addEventListener('DOMContentLoaded',async()=>{
   const layers=[
-    'polish.js?v=5.6.38',
+    'polish.js?v=5.6.39',
     'backup.js?v=5.6.4',
     'ux-5.6.3.js?v=5.6.3',
     'ui-5.6.4.js?v=5.6.23',
