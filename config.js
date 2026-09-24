9cce127eb8908e850854489532427cfe9470aef9
YanivBA10 <yanivba10@gmail.com>
2026-09-24 12:13:52 +0300

    Load polish layer v5.6.35

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ document.documentElement.classList.add('pt-ui-booting');
 
 window.addEventListener('DOMContentLoaded',async()=>{
   const layers=[
-    'polish.js?v=5.6.34',
+    'polish.js?v=5.6.35',
     'backup.js?v=5.6.2',
     'ux-5.6.3.js?v=5.6.3',
     'ui-5.6.4.js?v=5.6.22',
