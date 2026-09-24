dbb6eb461588bdf554dab228cdd4f8b68b0ffd39
YanivBA10 <yanivba10@gmail.com>
2026-09-24 12:41:11 +0300

    Load backup layer v5.6.3

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ document.documentElement.classList.add('pt-ui-booting');
 window.addEventListener('DOMContentLoaded',async()=>{
   const layers=[
     'polish.js?v=5.6.36',
-    'backup.js?v=5.6.2',
+    'backup.js?v=5.6.3',
     'ux-5.6.3.js?v=5.6.3',
     'ui-5.6.4.js?v=5.6.23',
     'tracker-ux-5.6.8.js?v=5.6.12',
